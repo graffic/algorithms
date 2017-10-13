5e6ce87a2e01616bc4499138a682291da86714f5
Javier Gonel <bolibic@gmail.com>
2017-10-13 20:49:16 +0300

    feat(cracking): chap 1 ex 2

---
+++ cracking_interview/ch1/q2_check_permutation.exs
@@ -0,0 +1,51 @@
+ExUnit.start()
+
+defmodule Ch1 do
+  @doc "Using sorting"
+  def permutation?(one, other) when length(one) != length(other), do: False
+  def permutation?(one, other) do
+    Enum.sort(String.graphemes one) == Enum.sort(String.graphemes other)
+  end
+
+  @doc "counting codepoints"
+  def permutation2?(one, other) when length(one) != length(other), do: False
+  def permutation2?(one, other), do: count(one) == count(other)
+  
+  def count(string, counter \\ %{})
+  def count("", counter), do: counter
+  def count(<<point::utf8, rest::bitstring>>, counter) do
+    counter = elem(Map.get_and_update(counter, point, fn
+      nil -> {nil, 1}
+      value -> {value, value + 1}
+    end), 1)
+    count(rest, counter)
+  end
+end
+
+defmodule Ch1Test do
+  use ExUnit.Case, async: true
+
+  test "Is permutation" do
+    assert Ch1.permutation?("abcd", "dcba")
+  end
+
+  test "Is not permutation length" do
+    assert !Ch1.permutation?("abcd", "dcb")
+  end
+
+  test "Is not permutation" do
+    assert !Ch1.permutation?("abcd", "dcbe")
+  end
+
+  test "Is permutation2" do
+    assert Ch1.permutation2?("abcd", "dcba")
+  end
+
+  test "Is not permutation2 length" do
+    assert !Ch1.permutation2?("abcd", "dcb")
+  end
+
+  test "Is not permutation2" do
+    assert !Ch1.permutation2?("abcd", "dcbe")
+  end
+end
---
+++ cracking_interview/ch1/q2_check_permutation.py
@@ -0,0 +1,55 @@
+"""
+Given two string, check if one is permutation of the other
+"""
+
+def check(one, other):
+    """
+    Using sorting ((a+b)log(a*b))
+
+    >>> check("abcda", "aadcb")
+    True
+    >>> check("abc", "bcd")
+    False
+    """
+    if len(one) == len(other):
+        return sorted(one) == sorted(other)
+    return False
+
+def count(string, max_char):
+    """
+    Count occurrences
+
+    >>> count("\x02\x03\x02", 5)
+    [0, 0, 2, 1, 0]
+    """
+    state = [0] * max_char
+    for char in string:
+        state[ord(char)] += 1
+    return state
+
+
+def check2(one, other):
+    """
+    Counting occurrences. 3(a+b) + c -> O(N)
+
+    >>> check2("abcda", "aadcb")
+    True
+    >>> check2("abc", "bc")
+    False
+    """
+    if len(one) != len(other):
+        return False
+    size = max(ord(x) for x in one + other) + 1
+
+    res_one = count(one, size)
+    res_other = count(other, size)
+
+    for i in range(size):
+        if res_one[i] != res_other[i]:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
